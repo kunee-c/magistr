2aedba14f5536f36f6378eafa86e78fa97068870
kunee <cedric3000@gmail.com>
2019-04-18 22:13:28 -0400

    (server) add mongoose dependencies and connexion

--- api/server.js
+++ api/server.js
@@ -2,6 +2,7 @@
 
 const express = require('express');
 const http = require('http');
+const mongoose = require('mongoose');
 
 // 1. Create main express intance
 const router = express();
@@ -16,27 +17,38 @@ const errorHandlers = require('./middleware/errorHandlers');
 
 // 4. Require routes
 const { router: bookRoutes } = require('./routes/books/bookRoutes');
+const { router: userRoutes } = require('./routes/users/userRoutes');
+const { router: adRoutes} = require('./routes/ads/adRoutes');
 
 // 5. Require conatants
-const { PORT } = require('./utils/constants');
+const { PORT, MONGO_DB_URL } = require('./utils/constants');
 
 // 6. Apply general middleware
 applyMiddleware(middleWare, router);
 
 // 7. Utilise routes
 router.use('/api/books', bookRoutes);
-
+router.use('/api/users', userRoutes);
+router.use('/api/ads', adRoutes);
 // 8. Apply error handling middleware (meaningfully last)
 applyMiddleware(errorHandlers, router);
-
 // 9. Create a server from express instance
 const server = http.createServer(router);
 
-// 10. Start server
-server.listen(PORT, () => {
-  console.log(`Server is running on PORT:${PORT}`);
-  if (process.send) {
-    // NOTE: process is being run by pm2
-    process.send('ready');
-  }
-});
+mongoose.connect(MONGO_DB_URL)
+    .then(() => {
+      console.log('Connected to mongodb instance');
+      // 10. Start server
+      server.listen(PORT, () => {
+        console.log(`Server is running on PORT:${PORT}`);
+        if (process.send) {
+          // NOTE: process is being run by pm2
+          process.send('ready');
+        }
+      });
+
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+
--- api/utils/constants.js
+++ api/utils/constants.js
@@ -2,3 +2,4 @@
 
 exports.PORT = process.env.PORT || 3001;
 exports.SECRET = process.env.SECRET || 'super-secret-passphrase';
+exports.MONGO_DB_URL = 'mongodb://localhost:27017/magistr';
