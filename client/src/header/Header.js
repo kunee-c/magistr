2c00afe159f334be63cc8f2751fff5f789d464d5
kunee <cedric3000@gmail.com>
2019-04-28 12:05:07 -0400

    (client) add link to go back to home

--- client/src/header/Header.js
+++ client/src/header/Header.js
@@ -2,22 +2,26 @@
  * Created by kunee on 18/04/2019.
  */
 import React from 'react';
-import {AppBar, Toolbar, IconButton, Typography, Button } from '@material-ui/core';
+import {AppBar, Toolbar, IconButton, Typography, Button} from '@material-ui/core';
 import MenuIcon from '@material-ui/icons/Menu';
-
+import HomeIcon from '@material-ui/icons/HomeOutlined';
+import {Link} from 'react-router-dom';
 
 export default props => {
-    const { btnclass, login } = props;
-    return(
-    <AppBar position="static">
-        <Toolbar>
-            <IconButton className={btnclass.menuButton} color="inherit" aria-label="Menu">
-                <MenuIcon />
-            </IconButton>
-            <Typography variant="h6" color="inherit" className={btnclass.grow}>
+    const {btnclass, login} = props;
+    return (
+        <AppBar position="static">
+            <Toolbar>
+                <IconButton className={btnclass.menuButton} color="inherit" aria-label="Menu">
+                    <MenuIcon />
+                </IconButton>
+                <Link to={`/`}>
+                    <HomeIcon color="action"/>
+                </Link>
+                <Typography variant="h6" color="inherit" className={btnclass.grow}>
 
-            </Typography>
-            <Button color="inherit" onClick={login}>Login</Button>
-        </Toolbar>
-    </AppBar>)
+                </Typography>
+                <Button color="inherit" onClick={login}>Login</Button>
+            </Toolbar>
+        </AppBar>)
 }
