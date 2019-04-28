3c66e5cb159bf20c6bab9f6a349caa98338e5766
kunee <cedric3000@gmail.com>
2019-04-28 12:05:56 -0400

    (client) add component to log in

--- client/src/login/Login.js
+++ client/src/login/Login.js
@@ -1,3 +1,50 @@
 /**
  * Created by kunee on 20/04/2019.
  */
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogTitle from '@material-ui/core/DialogTitle';
+
+export default class FormDialog extends React.Component {
+
+    render() {
+        const { open, handleLogin } = this.props;
+        return (
+            <div>
+                <Dialog
+                    open={open}
+                    onClose={handleLogin}
+                    aria-labelledby="form-dialog-title"
+                >
+                    <DialogTitle id="form-dialog-title">Login</DialogTitle>
+                    <DialogContent>
+                        <TextField
+                            autoFocus
+                            margin="dense"
+                            id="name"
+                            label="Email"
+                            type="email"
+                            fullWidth
+                        />
+                        <TextField
+                            margin="dense"
+                            id="name"
+                            label="Password"
+                            type="password"
+                            fullWidth
+                        />
+                    </DialogContent>
+                    <DialogActions>
+                        <Button onClick={this.handleClose} color="primary">
+                            Sign in
+                        </Button>
+                    </DialogActions>
+                </Dialog>
+            </div>
+        );
+    }
+}
