c2bfca08fccbab2a368de40e7240920f88c76e4d
kunee <cedric3000@gmail.com>
2019-04-28 12:16:01 -0400

    (conf) update env variable for heroku

--- api/utils/constants.js
+++ api/utils/constants.js
@@ -2,4 +2,4 @@
 
 exports.PORT = process.env.PORT || 3001;
 exports.SECRET = process.env.SECRET || 'super-secret-passphrase';
-exports.MONGO_DB_URL = 'mongodb://localhost:27017/magistr';
+exports.MONGO_DB_URL = process.env.MONGODB_URI || 'mongodb://localhost:27017/magistr';
