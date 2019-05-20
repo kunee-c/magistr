343766293da35baedd14ee7e5900492c602006d2
kunee <cedric3000@gmail.com>
2019-05-20 16:36:58 -0400

    (server) new route to get user location

--- api/routes/geolocation/GeoLocationRoute.js
+++ api/routes/geolocation/GeoLocationRoute.js
@@ -1,3 +1,21 @@
 /**
  * Created by kunee on 19/05/2019.
  */
+const express = require('express');
+const router = express.Router();
+var geoip = require('geoip-lite');
+
+router.route('/')
+    .get(async (req,res,next)=> {
+
+        var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+
+        if (ip === "127.0.0.1" || ip === "::ffff:127.0.0.1" || ip === "::1")
+            ip='99.239.198.0';
+
+        const geoData = geoip.lookup(ip);
+
+        res.status(200).json(geoData);
+    });
+
+exports.router = router;
