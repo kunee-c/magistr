028b0604fb21384a773e50f77f7d718cecce5f91
kunee <cedric3000@gmail.com>
2019-05-20 16:49:50 -0400

    (client) add google map react component to display teacher location

--- client/src/map/Map.js
+++ client/src/map/Map.js
@@ -1,3 +1,22 @@
 /**
  * Created by kunee on 04/05/2019.
  */
+import React, {Component} from 'react';
+import { withScriptjs, GoogleMap, Marker, withGoogleMap } from "react-google-maps"
+
+
+const Map =  withScriptjs(withGoogleMap((props) => {
+
+    console.log(props);
+    return (
+        <GoogleMap
+            defaultZoom={12}
+            defaultCenter={{ lat: props.lat, lng: props.lng }}
+            center={{ lat: props.lat, lng: props.lng }}
+        >
+            { <Marker position={{ lat: props.lat, lng: props.lng }} />}
+        </GoogleMap>
+    )
+}));
+
+export default Map;
